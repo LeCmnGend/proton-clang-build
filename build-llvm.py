86446b8306d50cbe9cf1a4cd19d1ba1bc106407f
Nathan Chancellor <nathan@kernel.org>
2022-01-10 08:46:20 -0700

    build-llvm.py: Appease new yapf
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>

--- build-llvm.py
+++ build-llvm.py
@@ -21,6 +21,7 @@ GOOD_REVISION = '4b1fde8a2b681dad2ce0c082a5d6422caa06b0bc'
 
 
 class Directories:
+
     def __init__(self, build_folder, install_folder, linux_folder, llvm_folder,
                  root_folder):
         self.build_folder = build_folder
@@ -31,6 +32,7 @@ class Directories:
 
 
 class EnvVars:
+
     def __init__(self, cc, cxx, ld):
         self.cc = cc
         self.cxx = cxx
